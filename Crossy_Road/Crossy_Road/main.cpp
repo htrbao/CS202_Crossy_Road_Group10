bb2a9159b0d49e9de4aed643d0552fbc0a22f308
htrbao <trbaohoo@gmail.com>
2021-11-01 19:24:08 +0700

    setUp

---
+++ Crossy_Road/Crossy_Road/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Network.hpp>
+
+using namespace std;
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+	cout << "Hello";
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # CS202_Crossy_Road_Group10
+
+##Description
+
+##Feature
+
+##Usage
+
+
+##Member
+- Hồ Trọng Bảo
+- Hà Thiên Lộc
+- Phạm Đình Khôi
+- Lê Quốc Cường
