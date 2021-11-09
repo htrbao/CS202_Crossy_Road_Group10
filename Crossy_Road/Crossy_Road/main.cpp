678ef9babdb3fc95f3d913023b92331efe01d80a
htrbao <trbaohoo@gmail.com>
2021-11-09 23:23:41 +0700

    CAT image

--- Crossy_Road/Crossy_Road/main.cpp
+++ Crossy_Road/Crossy_Road/main.cpp
@@ -11,13 +11,21 @@ using namespace std;
 #define SCREEN_WIDTH 473
 #define SCREEN_HEIGHT 957
 
+int dir = 1;
+
 int main() {
     sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Crossy Road");
-    sf::Texture imageSource;
-    imageSource.loadFromFile("assets/police_SE.png");
-    sf::Sprite imageSprite;
-    imageSprite.setTexture(imageSource);
-    imageSprite.scale(0.5, 0.5);
+    sf::Texture carTest;
+    carTest.loadFromFile("assets/firetruck_SE.png");
+    sf::Sprite carImageSprite;
+    carImageSprite.setTexture(carTest);
+    carImageSprite.scale(0.5, 0.5);
+ 
+    sf::Texture catTest;
+    catTest.loadFromFile("assets/CAT_1_NE.png");
+    sf::Sprite catImageSprite;
+    catImageSprite.setTexture(catTest);
+    catImageSprite.scale(0.25, 0.25);
 
     while (window.isOpen())
     {
@@ -27,9 +35,13 @@ int main() {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        imageSprite.move(0.05, 0.05);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+            carImageSprite.move(-0.05, 0.15);
+        }
+        carImageSprite.move(dir * 0.05, dir * 0.05);
         window.clear();
-        window.draw(imageSprite);
+        window.draw(carImageSprite);
+        window.draw(catImageSprite);
         window.display();
     }
 	cout << "Hello";
