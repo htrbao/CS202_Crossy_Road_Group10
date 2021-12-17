31d76b9ce1c259c98adc6cb72ce1c8a7907a44f1
htrbao <trbaohoo@gmail.com>
2021-12-17 15:12:14 +0700

    CROSSY sound

--- Crossy_Road/Crossy_Road/main.cpp
+++ Crossy_Road/Crossy_Road/main.cpp
@@ -2,6 +2,11 @@
 
 int main() {
 	CGAME game;
+	sf::SoundBuffer* buffer = &CASSET::GetInstance().soundMap["CROSSY"];
+	sf::Sound sound;
+	sound.setBuffer(*buffer);
+	sound.setLoop(true);
+	sound.play();
 	game.run();
 	return 0;
 }
