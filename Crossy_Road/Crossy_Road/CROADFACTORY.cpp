f02aae544ac7f069435331446a856436f05ca0f4
Hà Thiên Lộc <81582745+hathienloc131@users.noreply.github.com>
2021-12-20 11:59:52 +0700

    small fix

--- Crossy_Road/Crossy_Road/CROADFACTORY.cpp
+++ Crossy_Road/Crossy_Road/CROADFACTORY.cpp
@@ -79,10 +79,10 @@ void CROADFACTORY::draw(sf::RenderWindow& window)
 void CROADFACTORY::update(sf::RenderWindow& window)
 {
 	playSound();
-	if (mX > 1700)
+	if (mX > 1500)
 	{
 		cout << 1 << endl;
-		roadQueue.push_front(createRoad(roadQueue.front()->getPosition() + sf::Vector2f(100,100*tan(Constants::Alpha)), roadQueue.front()->is_road()));
+		roadQueue.push_front(createRoad(roadQueue.front()->getPosition() + sf::Vector2f(130,130*tan(Constants::Alpha)), roadQueue.front()->is_road()));
 		roadQueue.front()->draw(window);
 		mX = 0;
 	}
